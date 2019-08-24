6d2d9190afdeb4da87fd1bc111781a96e4126042
Dan Song <blueintelli@gmail.com>
2019-08-24 17:53:50 +0900

    Updates

---
+++ precache-manifest.509e43237b8dafee636cf11aba22fe6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380d16199e5d435ae4fd8c26943abdeb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "adce6aaa8a1d324f0a1e",
+    "url": "/movie_app_2019/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "80da972722621fe1b366",
+    "url": "/movie_app_2019/static/js/2.7153d5bd.chunk.js"
+  },
+  {
+    "revision": "adce6aaa8a1d324f0a1e",
+    "url": "/movie_app_2019/static/js/main.cff82e78.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
